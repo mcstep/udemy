5fc0393d0a7ac2bef829f0119fb7a9d9f602d42c
Stepan <mcstep777@gmail.com>
2021-04-13 17:37:47 +0700

    First app

--- Part 2/js/script.js
+++ Part 2/js/script.js
@@ -1,3 +1,19 @@
-let a=10;
-console.log(a++);
-console.log(a);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+
+const perconalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из просмотренных фильмов?',''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из просмотренных фильмов?',''),
+      d = prompt('На сколько оцените его?','');
+
+perconalMovieDB.movies[a] = b;
+perconalMovieDB.movies[c] = d;
+
+console.log(perconalMovieDB);
